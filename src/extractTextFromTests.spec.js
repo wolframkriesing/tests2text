6aadaa4d74cfa5c05bfadf438a820942101cd4d7
wolframkriesing <wolfram+github@kriesing.de>
2020-03-04 19:11:19 +0100

    Implement the skeleton of the parser.

--- src/extractTextFromTests.spec.js
+++ src/extractTextFromTests.spec.js
@@ -1,8 +1,17 @@
 import assert from 'assert';
 import {it, describe} from 'mocha';
 
+const extractTestSuites = s => {
+  return [];
+};
+
 describe('Extract the text from tests', () => {
-  it('do tests work?', () => {
-    assert(false);
+  describe('GIVEN a string', () => {
+    it('WHEN it is empty THEN return no test suites', () => {
+      assert.deepStrictEqual(extractTestSuites(''), []);
+    });
+    it('WHEN it contains not test THEN return no test suites', () => {
+      assert.deepStrictEqual(extractTestSuites('var x = 1; // but no test'), []);
+    });
   });
 });
