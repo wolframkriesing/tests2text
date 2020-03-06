9fc14aa9e4f7ba8de94dd81b44d295733004e8f5
wolframkriesing <wolfram+github@kriesing.de>
2020-03-06 07:47:13 +0100

    Use the typescript AST parser to parse test source code.

--- src/extractTextFromTests.spec.js
+++ src/extractTextFromTests.spec.js
@@ -1,7 +1,45 @@
 import assert from 'assert';
 import {it, describe} from 'mocha';
+import * as ts from 'typescript';
 
-const extractTestSuites = s => {
+const parseTestsOutOfSourceCode = (sourceCode) => {
+  const sourceFile = ts.createSourceFile(
+      "fileName",
+      sourceCode,
+      ts.ScriptTarget.ES2020,
+      true
+    );
+  return allTests(sourceFile);
+};
+
+const allTests = (sourceFile) => {
+  const nodes = [];
+  const childNodes = node => {
+    const nodes = [];
+    node.forEachChild(child => {
+        nodes.push(child);
+        return undefined;
+    });
+    return nodes;
+  };
+  let depth = 0;
+  const searchDescendants = node => {
+    const children = childNodes(node);
+    depth++;
+    for (const child of children) {
+      if (ts.isCallLikeExpression(child)) {
+        nodes.push(child.arguments[0].text);
+      }
+      searchDescendants(child);
+    }
+  };
+  searchDescendants(sourceFile);
+  return nodes;
+};
+
+const extractTestSuites = sourceCode => {
+  const tests = parseTestsOutOfSourceCode(sourceCode);
+  if (tests.length === 1) return tests;
   return [];
 };
 
@@ -12,6 +50,11 @@ describe('Extract the text from tests', () => {
     });
     it('WHEN it contains not test THEN return no test suites', () => {
       assert.deepStrictEqual(extractTestSuites('var x = 1; // but no test'), []);
+    });
+    describe('WHEN it contains one `describe`', () => {
+      it('THEN return one test suite', () => {
+        assert.strictEqual(extractTestSuites('describe("")').length, 1);
+      });
     });
   });
 });
