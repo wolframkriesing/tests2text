5b82d93a9a51c953c9f5dec34a6dafa40fe03bb9
wolframkriesing <wolfram+github@kriesing.de>
2020-03-07 19:55:20 +0100

    Render the tests too.

--- src/parseFile.js
+++ src/parseFile.js
@@ -9,10 +9,15 @@ const fullFileName = path.join(process.cwd(), fileName);
 const sourceCode = fs.readFileSync(fullFileName, 'utf8');
 const suites = extractTestSuites(sourceCode);
 
+const printTests = (tests, depth) => {
+  const prefix = new Array(depth).fill('  ').join('');
+  tests.forEach(test => console.log(prefix + test.name));
+};
 const printSuites = (suites, depth = 0) => {
   const prefix = new Array(depth).fill('  ').join('');
   suites.forEach(suite => {
     console.log(prefix + suite.name);
+    printTests(suite.tests, depth + 1);
     suite.suites ? printSuites(suite.suites, depth + 1) : null;
   });
 };
