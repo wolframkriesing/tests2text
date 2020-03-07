218ddfd31f77248d3e8d5498b88fdc8b460924e4
wolframkriesing <wolfram+github@kriesing.de>
2020-03-07 16:46:40 +0100

    Built a very simple parse-file script, which dumps the test descriptions of a file.

---
+++ src/parseFile.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import fs from 'fs';
+import {extractTestSuites} from './extractTextFromTests.js';
+
+const allCommandLineArgs = process.argv;
+const indexOfFileName = allCommandLineArgs.findIndex(arg => arg === __filename) + 1;
+const fileName = allCommandLineArgs[indexOfFileName];
+const fullFileName = path.join(process.cwd(), fileName);
+console.log("fullFileName= ", fullFileName);
+const sourceCode = fs.readFileSync(fullFileName, 'utf8');
+const suites = extractTestSuites(sourceCode);
+console.log(JSON.stringify(suites, null, 2));
