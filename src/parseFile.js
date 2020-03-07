82b4d6093c87a21d04f499e6cfe4857099179c56
wolframkriesing <wolfram+github@kriesing.de>
2020-03-07 16:51:33 +0100

    Cleaning up a bit.

--- src/parseFile.js
+++ src/parseFile.js
@@ -6,7 +6,7 @@ const allCommandLineArgs = process.argv;
 const indexOfFileName = allCommandLineArgs.findIndex(arg => arg === __filename) + 1;
 const fileName = allCommandLineArgs[indexOfFileName];
 const fullFileName = path.join(process.cwd(), fileName);
-console.log("fullFileName= ", fullFileName);
 const sourceCode = fs.readFileSync(fullFileName, 'utf8');
 const suites = extractTestSuites(sourceCode);
+
 console.log(JSON.stringify(suites, null, 2));
