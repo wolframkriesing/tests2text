ee07437f52c598246010fb78e94b7239fa31236f
wolframkriesing <wolfram+github@kriesing.de>
2020-03-09 21:48:18 +0100

    Prevent from reading files larger than 2MB, just some simple basic protection to not crash my server.

--- src/parseFile.js
+++ src/parseFile.js
@@ -8,6 +8,7 @@ const readFromLocalFilesystem = (fileName) => {
   const sourceCode = fs.readFileSync(fullFileName, 'utf8');
   return sourceCode;
 };
+const readSizeLimit = 2 * 1024 * 1024; // 1MB = 1024 * 1024
 const readFromWeb = (url) => {
   return new Promise((resolve, reject) => {
     https.get(url, (res) => {
@@ -18,7 +19,12 @@ const readFromWeb = (url) => {
       }
       res.setEncoding('utf8');
       let rawData = '';
-      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('data', (chunk) => {
+        rawData += chunk;
+        if (rawData.length > readSizeLimit) {
+          res.destroy(Error(`Stop receiving, size limit (${readSizeLimit}) reached.`));
+        }
+      });
       res.on('end', () => {resolve(rawData);});
     }).on('error', (e) => {
       reject('error reading file: ' + e.message);
