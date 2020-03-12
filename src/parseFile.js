3bfed29e6db3b55a3905628b42d02269ae59f7c2
wolframkriesing <wolfram+github@kriesing.de>
2020-03-12 23:16:07 +0100

    Show the stats on the console.

--- src/parseFile.js
+++ src/parseFile.js
@@ -1,18 +1,23 @@
 import {extractTextFromFile} from './extractTextFromFile.js';
+import {stats} from './stats.js';
 
 const printTests = (tests, depth) => {
   const prefix = new Array(depth).fill('  ').join('');
   tests.forEach(test => console.log(prefix + test.name));
 };
-const printTestSuites = (suites, depth = 0) => {
+const printTestSuites = (all, depth = 0) => {
+  const {suites} = all;
   const prefix = new Array(depth).fill('  ').join('');
   suites.forEach(suite => {
     console.log(prefix + suite.name);
     printTests(suite.tests, depth + 1);
-    suite.suites ? printTestSuites(suite.suites, depth + 1) : null;
+    suite.suites ? printTestSuites(suite, depth + 1) : null;
   });
 };
-
+const printStats = (stats) => {
+  console.log("\nStatistics\n-----------");
+  console.log(JSON.stringify(stats, null, 4));
+};
 const allCommandLineArgs = process.argv;
 const indexOfFileName = allCommandLineArgs.findIndex(arg => arg === __filename) + 1;
 const fileName = allCommandLineArgs[indexOfFileName];
@@ -26,4 +31,5 @@ const fileName = allCommandLineArgs[indexOfFileName];
     return;
   }
   printTestSuites(testSuites);
+  printStats(stats(testSuites));
 })();
